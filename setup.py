9d156d7d0339c3db94320863b86ae98c1f73420a
Taro Kiritani <taro.kiritani@exwzd.com>
2021-03-19 11:35:42 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name="pytorch_cifar",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=["Programming Language :: Python :: 3.6",],
+)
